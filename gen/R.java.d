1ef767e968dfd456334c37c63374c3dea2cdfc5a
Prateek Jain <prateek.jain23995@yahoo.com>
2015-07-09 17:36:20 +0530

    basic application created

---
+++ gen/R.java.d
@@ -0,0 +1,9 @@
+/Users/prateekj/self_Practise/Android/Pushup_Counter/gen/com/prateekj/R.java \
+ : /Users/prateekj/self_Practise/Android/Pushup_Counter/res/drawable-hdpi/ic_launcher.png \
+/Users/prateekj/self_Practise/Android/Pushup_Counter/res/drawable-ldpi/ic_launcher.png \
+/Users/prateekj/self_Practise/Android/Pushup_Counter/res/drawable-ldpi/round_button.xml \
+/Users/prateekj/self_Practise/Android/Pushup_Counter/res/drawable-mdpi/ic_launcher.png \
+/Users/prateekj/self_Practise/Android/Pushup_Counter/res/drawable-xhdpi/ic_launcher.png \
+/Users/prateekj/self_Practise/Android/Pushup_Counter/res/layout/main.xml \
+/Users/prateekj/self_Practise/Android/Pushup_Counter/res/values/strings.xml \
+/Users/prateekj/self_Practise/Android/Pushup_Counter/bin/AndroidManifest.xml \
---
+++ src/com/prateekj/MyActivity.java
@@ -0,0 +1,25 @@
+package com.prateekj;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.lang.Integer;
+
+public class MyActivity extends Activity
+{
+    private Integer counter = 0;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    public void increaseCounter(View view) {
+        TextView counterField = (TextView)findViewById(R.id.counter);
+        counter++;
+        counterField.setText(counter.toString());
+    }
+}
