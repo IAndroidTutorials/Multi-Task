38436a79ac4c979e923217828d21f48bd1cb5fb7
Prateek Jain <prateek.jain23995@yahoo.com>
2015-07-17 10:59:54 +0530

    functionality implemented to delete the tasks

--- src/com/prateekj/activity/ListToDoActivity.java
+++ src/com/prateekj/activity/ListToDoActivity.java
@@ -4,8 +4,15 @@ import android.app.Activity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.prateekj.helper.DbHelper;
 import com.prateekj.R;
@@ -14,7 +21,13 @@ import com.prateekj.schema.ToDoContract;
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.widget.Toast.LENGTH_SHORT;
+import static java.lang.String.valueOf;
+
 public class ListToDoActivity extends Activity {
+
+    private TextView currentlySelectedTask;
+    private ArrayAdapter<String> taskListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,8 +64,34 @@ public class ListToDoActivity extends Activity {
     }
 
     private void showTasks() {
-        ArrayAdapter<String> tasksAdapter = new ArrayAdapter<String>(this, R.layout.todo_list_text_view, retrieveTasks());
+        this.taskListAdapter = new ArrayAdapter<String>(this, R.layout.todo_list_text_view, retrieveTasks());
         ListView taskListView = (ListView) findViewById(R.id.task_list);
-        taskListView.setAdapter(tasksAdapter);
+        taskListView.setAdapter(this.taskListAdapter);
+        registerForContextMenu(taskListView);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        this.currentlySelectedTask = (TextView) ((AdapterView.AdapterContextMenuInfo) menuInfo).targetView;
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.todo_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        CharSequence text = this.currentlySelectedTask.getText();
+        deleteToDoFor(text.toString());
+        return super.onContextItemSelected(item);
+    }
+
+    private void deleteToDoFor(String task) {
+        DbHelper dbHelper = new DbHelper(this);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        String selection = ToDoContract.ToDo.TASK + " LIKE ?";
+        String[] selectionArgs = {task};
+        db.delete(ToDoContract.ToDo.TABLE_NAME, selection, selectionArgs);
+        this.taskListAdapter.remove(task);
+        Toast.makeText(this, "Task Deleted", LENGTH_SHORT).show();
     }
 }
